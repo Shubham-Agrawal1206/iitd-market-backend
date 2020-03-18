e2f60f4489eff15aaa664f170a48db00afe083e4
shubham1206agra <tt1191044@iitd.ac.in>
2020-03-18 15:08:36 +0530

    added reset option

--- routes/index.js
+++ routes/index.js
@@ -108,13 +108,13 @@ router.post('/forgot', function(req, res, next) {
         var smtpTransport = nodemailer.createTransport({
           service: 'Gmail', 
           auth: {
-            user: 'see9677@gmail.com',
+            user: 'emailxx365@gmail.com',
             pass: process.env.GMAILPW
           }
         });
         var mailOptions = {
           to: user.email,
-          from: 'see9677@gmail.com',
+          from: 'emailxx365@gmail.com',
           subject: 'Node.js Password Reset',
           text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
             'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
@@ -172,13 +172,13 @@ router.post('/reset/:token', function(req, res) {
         var smtpTransport = nodemailer.createTransport({
           service: 'Gmail', 
           auth: {
-            user: 'see9677@gmail.com',
+            user: 'emailxx365@gmail.com',
             pass: process.env.GMAILPW
           }
         });
         var mailOptions = {
           to: user.email,
-          from: 'see9677@mail.com',
+          from: 'emailxx365@mail.com',
           subject: 'Your password has been changed',
           text: 'Hello,\n\n' +
             'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
