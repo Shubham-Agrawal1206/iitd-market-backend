4169487c0bb5f4c6e21b33bc2d8b07fd2c843c5e
shubham1206agra <tt1191044@iitd.ac.in>
2020-03-18 12:50:03 +0530

    user profile

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campground");
 var pathx = require("path");
 
 //root route
@@ -13,6 +14,23 @@ router.get("/", function(req, res){
 router.get("/register", function(req, res){
    res.render("register", {page: 'register'}); 
 });
+
+//User Profile
+router.get("/users/:id",function(req,res){
+    User.findById(req.params.id,function(err,foundUser){
+        if(err){
+            req.flash("error","Something went wrong");
+            return res.redirect("/");
+        }
+        Campground.find().where('author.id').equals(foundUser._id).exec(function(err,foundCamp){
+        if(err){
+            req.flash("error","Something went wrong");
+            return res.redirect("/");
+        }
+        res.render("users/show",{user:foundUser,campgrounds:foundCamp});   
+        })   
+    })
+})
 
 //handle sign up logic
 router.post("/register", function(req, res){
